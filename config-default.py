db45ea192ebfa5877b36a6752997b857da8b0e78
hzhu212 <zhuhe212@163.com>
2017-09-10 11:09:06 +0800

    use cmd library

--- config-default.py
+++ config-default.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 
-current = "src1"
+current = "gg"
 
 sources = {
-    "src1": "https://raw.githubusercontent.com/googlehosts/hosts/master/hosts-files/hosts",
-    "src2": "https://coding.net/u/scaffrey/p/hosts/git/raw/master/hosts-files/hosts",
-    "src3": "https://raw.githubusercontent.com/racaljk/hosts/master/hosts"
+    "gg": "https://raw.githubusercontent.com/googlehosts/hosts/master/hosts-files/hosts",
+    "gg-m": "https://coding.net/u/scaffrey/p/hosts/git/raw/master/hosts-files/hosts",
+    "lengers": "http://git.oschina.net/lengers/connector/raw/master/hosts",
+    "ll-ipv6": "https://raw.githubusercontent.com/lennylxx/ipv6-hosts/master/hosts",
+    "racaljk": "https://raw.githubusercontent.com/racaljk/hosts/master/hosts",
+    "sly": "https://raw.githubusercontent.com/superliaoyong/hosts/master/hosts",
+    "sy618-fq": "https://raw.githubusercontent.com/sy618/hosts/master/FQ",
+    "sy618-pc": "https://raw.githubusercontent.com/sy618/hosts/master/pc",
+    "vokins-ad": "https://raw.githubusercontent.com/vokins/yhosts/master/hosts",
+    "wcm": "https://raw.githubusercontent.com/wangchunming/2017hosts/master/hosts-pc",
+    "wcm-ipv6": "https://raw.githubusercontent.com/wangchunming/2017hosts/master/hosts-ipv6-pc"
 }
--- hman.py
+++ hman.py
@@ -6,6 +6,7 @@ import os
 import sys
 import platform
 import json
+from cmd import Cmd
 
 help_message = '''
 Usage:
@@ -13,34 +14,128 @@ Usage:
 
 Commands:
   help:   \tShow this help message. Equivalent to "-h", "/?", "--help".
-  run:    \tUpdate hosts using current source.
   list:   \tList all the sources available. Source in use starts with "*".
-  add:    \tAdd a new source. Usage: hman add source_name source_url.
-  remove: \tRemove an existing source. Usage: hman remove source_name.
-  use:    \tChange current source. Usage: hman use source_name.
-  rename: \tRename a source. Usage: hman rename old_name new_name.
+  pull:   \t<[source_name]>. Pull and store hosts from specified source or current source, but not use!
+  use:    \t<source_name>. Use specified source to replace system hosts.
+  update: \t<[source_name]>. Pull and use hosts from specified source or current source. Same as pull && use.
+  add:    \t<source_name source_url>. Add new source.
+  remove: \t<source_name>. Remove an existing source.
+  rename: \t<old_name new_name>. Rename source.
   ls:     \tSame as list.
   rm:     \tSame as remove.
 '''
 
-def update_config(update_dict):
-    import config
-    keys = [k for k in dir(config) if not k.startswith('__')]
-    values = [config.__dict__.get(k, None) for k in keys]
-    config_dict = dict(zip(keys, values))
-    config_dict.update(update_dict)
-    config_file = os.path.join(os.path.dirname(__file__), 'config.py')
-    with open(config_file, 'w') as f:
-        f.write('# -*- coding: utf-8 -*-\n')
-        for k, v in sorted(config_dict.items()):
-            s = '\n{name} = {value}\n'.format(name=k, value=json.dumps(v, indent=4, sort_keys=True))
-            f.write(s)
+class HostsManager(Cmd):
+    def __init__(self):
+        Cmd.__init__(self)
+        self.prompt = '(hman)>> '
+        try:
+            import config
+        except ImportError:
+            config = __import__('config-default')
+        self.sources = config.sources
+        self.current = config.current
+
+    def _rewrite_config(self):
+        config_dict = { "current": self.current, "sources": self.sources }
+        app_root = os.path.dirname(os.path.abspath(sys.modules[self.__module__].__file__))
+        config_file = os.path.join(app_root, 'config.py')
+        with open(config_file, 'w') as f:
+            f.write('# -*- coding: utf-8 -*-\n')
+            for k, v in sorted(config_dict.items()):
+                s = '\n{name} = {value}\n'.format(name=k, value=json.dumps(v, indent=4, sort_keys=True))
+                f.write(s)
+
+    def default(self, line):
+        Cmd.default(self, line)
+        print(help_message)
+
+    def do_rewrite_config(self, line):
+        """Rewrite config.py manually.
+        Generally, hman will rewrite it automaticly after config changed.\n"""
+        self._rewrite_config()
+
+    def do_list(self, line):
+        """List all the hosts sources. Symbol "*" indicates the one in use.
+        Use "-a" or "--all" to list detail information.\n"""
+        for index, src in enumerate(self.sources):
+            lead = '*' if src['name'] == self.current else ' '
+            if line in ('-a', '--all'):
+                print('{lead} [{index}]\tname: {name}\n\turl: {url}\n\tnote: {note}'.format(
+                    lead=lead, index=index, name=src['name'], url=src['url'], note=src['note']))
+            else:
+                print('{lead} {name}:\t{url}'.format(lead=lead, name=src['name'], url=src['url']))
+        print()
+
+    do_ls = do_list
+
+    def do_add(self, line):
+        """Add a new source.
+        Usage: add name url [note].\n"""
+        params = line.split()
+        if len(params) < 2:
+            print('*** Error: require "name" and "url" of the source to add.\n')
+            return
+        name, url = params[:2]
+        note = params[2] if len(params)>2 else ''
+        self.sources.append({ 'name': name, 'url': url, 'note': note })
+        print('Added new source: %s - %s\n' %(name, url))
+
+    def do_remove(self, line):
+        """Remove source(s).
+        Usage: remove name [name2 [name3]]...
+        Use alias "rm" for simplicity.\n"""
+        names = line.split()
+        if not names:
+            print('*** Error: require at least one source name.\n')
+            return
+        all_names = [src['name'] for src in self.sources]
+        for name in names:
+            if name not in all_names:
+                print('*** Error: unknown source name "%s".\n' %name)
+                return
+        self.sources = [src for src in self.sources if src['name'] not in names]
+        print('Removed source(s): %s\n' %(' '.join(names)))
+
+    do_rm = do_remove
+
+    def do_rename(self, line):
+        """Rename a source.
+        Usage: rename old_name new_name.\n"""
+        params = line.split()
+        if len(params) < 2:
+            print('*** Error: require old_name and new_name.\n')
+            return
+        old_name, new_name = params[:2]
+        all_names = [src['name'] for src in self.sources]
+        if old_name not in all_names:
+            print('*** Error: unknown source name "%s".\n' %old_name)
+            return
+        self.sources[all_names.index(old_name)]['name'] = new_name
+        print('Renamed "%s" to "%s"\n' %(old_name, new_name))
+
+    def do_reorder(self, line):
+        pass
+
+    def do_pull(self, line):
+        pass
+
+    def do_use(self, line):
+        pass
+
+    def do_update(self, line):
+        pass
+
+    def do_exit(self, line):
+        """Exit hosts-manager.\n"""
+        self._rewrite_config()
+        sys.exit(0)
 
 
-if __name__ == '__main__':
+"""if __name__ == '__main__':
     command = sys.argv[1] if len(sys.argv) > 1 else None
 
-    if command == 'run':
+    if command == 'pull':
         script = os.path.join(os.path.dirname(__file__), 'updator.py')
         system = platform.system()
 
@@ -113,4 +208,8 @@ if __name__ == '__main__':
         print(help_message)
     else:
         print('Unknown command: "%s"\n' %(' '.join(sys.argv)))
-        print(help_message)
+        print(help_message)"""
+
+if __name__ == '__main__':
+    hman = HostsManager()
+    hman.cmdloop()
